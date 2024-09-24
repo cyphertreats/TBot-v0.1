d957fc307c221efa94f77bad03aa456e0a217280
Sanchit Manhas <103139058+cyphertreats@users.noreply.github.com>
2024-09-24 13:48:46 +0530

    Update web_interface.py

--- web_interface.py
+++ web_interface.py
@@ -1,15 +1,20 @@
+# web_interface.py
+
 from flask import Flask, render_template, jsonify
-import data_fetcher
-import model
+import main_operations  # Import the new operations module
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    hist_data = data_fetcher.get_historical_data()
-    trained_model = model.train_model(hist_data)
-    future_prediction = model.predict_future(trained_model)
-    return render_template('index.html', predictions=future_prediction)
+    hist_data = main_operations.get_historical_data()  # Call from main_operations
+    trained_model = main_operations.train_model(hist_data)
+    future_prediction = main_operations.predict_future(trained_model)
+
+    # Convert future_prediction from NumPy array to list
+    future_prediction_list = future_prediction.tolist()
+
+    return render_template('index.html', predictions=future_prediction_list)
 
 if __name__ == '__main__':
     app.run(debug=True)
